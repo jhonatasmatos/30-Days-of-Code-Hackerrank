91fcd5f76f0ea0a33d8ad897a3394961427c63fc
jhow <jho_santos_jho@hotmail.com>
2021-10-16 21:26:30 -0300

    added Day 7 Arrays solution

---
+++ CSharpCodes/Day7Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day7Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            for (var i = n - 1; i > -1; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
